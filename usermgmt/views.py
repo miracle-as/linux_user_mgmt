6c1a4d7e1f2054c4cbc482a76414ddcc50b93d55
Martin Juhl <m@rtinjuhl.dk>
2021-04-13 10:59:41 +0200

    Fixes

--- usermgmt/views.py
+++ usermgmt/views.py
@@ -37,7 +37,7 @@ def addsuccess(request):
                 break
         password = password
         encpass = crypt.crypt(password, '22')
-        usercheck = os.system("sudo useradd "+username+" -p "+encpass+" -m -s /bin/bash -e "+expirydate+" -G internal-sftp")
+        usercheck = os.system("sudo useradd "+username+" -p "+encpass+" -m -s /bin/bash -e "+expirydate+" -G sftpusers")
 
         if userexist == username:
            print("User already exist: %s" %username)
